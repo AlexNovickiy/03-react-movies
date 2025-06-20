fa67ce8e175fb19bbacaa8e98948b08773baefdd
AlexNovickiy <novickiisasha78@gmail.com>
2025-06-20 23:49:50 +0300

    Update package dependencies and integrate React Query for state management

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,18 @@ import App from './components/App/App';
 import 'modern-normalize';
 // Глобальні стилі
 import './global.css';
+// src/main.tsx
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
